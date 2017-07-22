ae106a12f596f7f7d7b10d4a9e225f7f6654610d
hangneu <shangneu1992@gmail.com>
2017-07-22 16:13:24 -0400

    Update 162.py

--- 162.py
+++ 162.py
@@ -1,2 +1,21 @@
 class Solution(object):
-	pass
+    def findPeakElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        start, end = 0, len(nums) - 1
+        while start + 1 < end:
+            middle = (end - start)/2 + start
+            if nums[middle] > nums[middle-1] and nums[middle] >nums[middle+1]:
+                return middle
+            elif nums[middle] < nums[middle-1] and nums[middle] < nums[middle+1]:
+                end = middle
+            elif nums[middle] < nums[middle - 1] and nums[middle] > nums[middle+1]:
+                end = middle
+            else:
+                start = middle
+        if nums[start] > nums[end]:
+            return start
+        else:
+            return end
