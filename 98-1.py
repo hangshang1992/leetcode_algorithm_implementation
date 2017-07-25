80c3c6c98fd4dfbca013787501c41863f73015e1
hangneu <shangneu1992@gmail.com>
2017-07-24 21:09:51 -0400

    Create 98-1.py

---
+++ 98-1.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        res, stack = [], []
+        while root or stack:
+            if root:
+                stack.append(root)
+                root = root.left
+            else:
+                node = stack.pop()
+                res.append(node.val)
+                root = node.right
+        for i in range(1,len(res)):
+            if res[i-1] >= res[i]:
+                return False
+        return True
