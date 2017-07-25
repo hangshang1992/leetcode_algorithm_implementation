ef999dd14805fd28d614e0d8a72ca4b63e4f8159
hangneu <shangneu1992@gmail.com>
2017-07-25 15:37:40 -0400

    Update 31.py

--- 31.py
+++ 31.py
@@ -4,24 +4,24 @@ class Solution(object):
         :type nums: List[int]
         :rtype: void Do not return anything, modify nums in-place instead.
         """
-        right = len(nums) - 1
-        if len(nums) <= 1: 
+        if len(nums)<=1:
             nums[:] = nums
         else:
-            while right > 0 and nums[right] <= nums[right - 1]:
-                right -= 1
-            if right == 0:
+            left = len(nums) - 1
+            while left > 0 and nums[left] <=  nums[left - 1]:
+                left -= 1
+            if left == 0:
                 return self.reverse(nums, 0, len(nums) - 1)
-            right -= 1
-            left = 0
-            for i in range(len(nums)-1, right, -1):
-                if nums[i] > nums[right]:
-                    left = i
+            left -= 1
+            right = 0
+            for i in range(len(nums)-1, left, -1):
+                if nums[i] > nums[left]:
+                    right = i
                     break
-            nums[right], nums[left] = nums[left], nums[right]
-            self.reverse(nums, right + 1, len(nums) - 1)
+            nums[left], nums[right] = nums[right], nums[left]
+            self.reverse(nums, left + 1, len(nums)-1)
     def reverse(self, nums, l, r):
-        while l<r: 
+        while l<r:
             nums[l], nums[r] = nums[r], nums[l]
             l += 1
             r -= 1
