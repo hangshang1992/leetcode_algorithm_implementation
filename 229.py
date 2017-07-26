9841074fb71659da20eb613104beb07527aaf6a9
hangneu <shangneu1992@gmail.com>
2017-07-26 15:11:59 -0400

    Update 229.py

--- 229.py
+++ 229.py
@@ -4,33 +4,30 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
+        count1, count2, majority1, majority2 = 0, 0, "", ""
+        for item in nums:
+            if count1 > 0 and majority1 == item:
+                count1 += 1
+                continue
+            if count2 > 0 and majority2 == item:
+                count2 += 1
+                continue
+            if count1 == 0:
+                majority1 = item
+                count1 += 1
+                continue
+            if count2 == 0:
+                majority2 = item
+                count2 += 1
+                continue
+            count1 -= 1
+            count2 -= 1
         result = []
-        x = [float('inf'),float('inf')]
-        cnt = [0,0]
-        length = len(nums)
-        for i in range(length):
-            if cnt[0] > 0 and x[0] == nums[i]:
-                cnt[0] += 1
-                continue
-            if cnt[1] > 0 and x[1] == nums[i]:
-                cnt[1] += 1
-                continue
-            if cnt[0] == 0:
-                x[0] = nums[i]
-                cnt[0] = 1
-                continue
-            if cnt[1] == 0:
-                x[1] = nums[i]
-                cnt[1] = 1
-                continue
-            cnt[0] -= 1
-            cnt[1] -= 1
-        for i in range(2):
-            if cnt[i] > 0:
-                count = 0
-                for item in nums:
-                    if x[i] == item:
-                        count += 1
-                if count > length/3:
-                    result.append(x[i])
+        for item in [majority1, majority2]:
+            count = 0
+            for item1 in nums:
+                if item == item1:
+                    count += 1
+            if count > len(nums)/3 and item not in result:
+                result.append(item)
         return result
