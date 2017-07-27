a70c6fa9f88ab872e02a4cdfa58ddbe409c1eb80
hangneu <shangneu1992@gmail.com>
2017-07-26 21:28:37 -0400

    Create 67.py

---
+++ 67.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        index_a = len(a) - 1
+        index_b = len(b) - 1
+        carry = 0
+        sum = ""
+        while index_a >=0 or index_b >= 0:
+            x = int(a[index_a]) if index_a >= 0 else 0
+            y = int(b[index_b]) if index_b >= 0 else 0
+            carry, val = divmod(x+y+carry,2)
+            if val == 1:
+                sum = '1' + sum
+            else:
+                sum = '0' + sum
+            index_a -= 1
+            index_b -= 1
+        if carry == 1:
+            sum = '1' + sum
+        return sum
