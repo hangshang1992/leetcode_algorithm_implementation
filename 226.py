b636d676904fb2d8521a231da45f7dd436158f82
hangneu <shangneu1992@gmail.com>
2017-07-27 21:42:26 -0400

    Create 226.py

---
+++ 226.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if node:
+                node.left, node.right = node.right, node.left
+                stack.append(node.right)
+                stack.append(node.left)
+        return root
