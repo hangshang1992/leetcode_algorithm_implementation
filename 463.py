cf29018251455b790024315b3cba9ac2615112bc
hangneu <shangneu1992@gmail.com>
2017-07-30 10:22:28 -0400

    Create 463.py

---
+++ 463.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def islandPerimeter(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        result = 0
+        m = len(grid)
+        n = len(grid[0])
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    if i==0 or grid[i-1][j] == 0: 
+                        result += 1
+                    if i==m-1 or grid[i+1][j] == 0: 
+                        result += 1
+                    if j==0 or grid[i][j-1]== 0: 
+                        result += 1
+                    if j == n-1 or grid[i][j+1]==0: 
+                        result += 1
+        return result
