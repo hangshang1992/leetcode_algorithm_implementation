b3eadd56c7c15da7c4dfad94131fd29d672b69d6
hangneu <shangneu1992@gmail.com>
2017-07-30 10:03:51 -0400

    Create 189.py

---
+++ 189.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        length = len(nums)
+        k %= length
+        nums[:] = nums[-k:] + nums[:-k]
+        
