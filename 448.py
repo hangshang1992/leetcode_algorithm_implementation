bcca328abea449660a1d977b05bec0b515ca721e
hangneu <shangneu1992@gmail.com>
2017-07-31 10:41:08 -0400

    Create 448.py

---
+++ 448.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def findDisappearedNumbers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        nums = [0] + nums
+        for i in range(len(nums)):
+            index = abs(nums[i])
+            nums[index] = -1 * abs(nums[index])
+        return [i for i in range(len(nums)) if nums[i] > 0]
