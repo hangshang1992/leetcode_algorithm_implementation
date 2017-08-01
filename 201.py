9be908f73420b1065b70c826d209f3047d0f8dde
hangneu <shangneu1992@gmail.com>
2017-08-01 12:37:11 -0400

    Create 201.py

---
+++ 201.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def rangeBitwiseAnd(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        i = 0
+        while m!=n:
+            n >>= 1
+            m >>= 1
+            i += 1
+        return n<<i
