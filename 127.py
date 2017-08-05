58cf78ea72f94d6f7c0966d370de2777e5858bfd
hangneu <shangneu1992@gmail.com>
2017-08-04 21:47:32 -0400

    Create 127.py

---
+++ 127.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def ladderLength(self, start, end, wordList):
+        wordSet = set([])
+        for word in wordList:
+            wordSet.add(word)
+
+        wordLen = len(start)
+        queue = collections.deque([(start, 1)])
+        while queue:
+            currWord, currLen = queue.popleft()
+            if currWord == end:
+                return currLen
+            for i in xrange(wordLen):
+                part1 = currWord[:i]
+                part2 = currWord[i+1:]
+                for j in 'abcdefghijklmnopqrstuvwxyz':
+                    if currWord[i] != j:
+                        nextWord = part1 + j + part2
+                        if nextWord in wordSet:
+                            queue.append((nextWord, currLen + 1))
+                            wordSet.remove(nextWord)
+        return 0
