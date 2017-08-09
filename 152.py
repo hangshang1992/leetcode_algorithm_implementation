235eee1497a097be580b6d05a9ee07dc599d7387
hangneu <shangneu1992@gmail.com>
2017-08-09 15:20:54 -0400

    Update 152.py

--- 152.py
+++ 152.py
@@ -4,10 +4,8 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        f, g = [], []
-        f.append(nums[0])
-        g.append(nums[0])
-        for i in xrange(1, len(nums)):
+        f, g= [nums[0]], [nums[0]]
+        for i in range(1, len(nums)):
             f.append(max(f[i-1]*nums[i], g[i-1]*nums[i], nums[i]))
             g.append(min(f[i-1]*nums[i], g[i-1]*nums[i], nums[i]))
         return max(f)
