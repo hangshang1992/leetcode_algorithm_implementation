2cc98b70d621915bc3c610cc681f8c7535ec6bc5
hangneu <shangneu1992@gmail.com>
2017-08-20 11:23:35 -0400

    Update 187-1.py

--- 187-1.py
+++ 187-1.py
@@ -12,3 +12,17 @@ class Solution(object):
             else:
                 dic[s[i:i+10]] = 0
         return res
+class Solution(object):
+    def findRepeatedDnaSequences(self, s):
+        """
+        :type s: str
+        :rtype: List[str]
+        """
+        set1 = set()
+        res = []
+        for i in range(len(s)-9):
+            if s[i:i+10] in set1 and s[i:i+10] not in res:
+                res.append(s[i:i+10])
+            else:
+                set1.add(s[i:i+10])
+        return res
