f114cf4cb122b5d39f6ee45d96c5a6ee34c0f45e
hangneu <shangneu1992@gmail.com>
2017-08-25 12:27:39 -0400

    Create 265.py

---
+++ 265.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def minCostII(self, costs):
+        """
+        :type costs: List[List[int]]
+        :rtype: int
+        """
+        n = len(costs)
+        if n == 0:
+            return 0
+        k = len(costs[0])
+        if k == 1:
+            return costs[0][0]
+        res = [[float('inf') for _ in range(k)] for _ in range(n + 1)]
+        for i in range(k):
+            res[0][i] = 0
+        for i in range(1, n+1):
+            for j in range(k):
+                for x in range(k):
+                    if j != x:
+                        res[i][j] = min(res[i][j], res[i-1][x] + costs[i-1][j])
+        return min(res[n])
