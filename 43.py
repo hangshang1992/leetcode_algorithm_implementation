e858f420d4a79dddeb430a2d246690fe490fe4b0
hangneu <shangneu1992@gmail.com>
2017-09-02 16:17:15 -0400

    Update 43.py

--- 43.py
+++ 43.py
@@ -14,3 +14,20 @@ class Solution(object):
             res[i] += carry
         res = ''.join(map(str,res))
         return '0' if not res.lstrip("0") else res.lstrip("0")
+
+   '''
+   class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        res = [0]*(len(num1)+len(num2))
+        for i in range(len(num1) - 1, -1, -1):
+            carry = 0
+            for j in range(len(num2) - 1, -1, -1):
+                carry, res[i+j+1] = divmod(int(num1[i])*int(num2[j]) + carry + res[i+j+1], 10)
+            res[i] += carry
+        return ''.join(map(str, res)).lstrip('0') or '0'
+   '''
