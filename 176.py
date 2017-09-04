11236e9c597d25eb176e03b20287b52d24e58535
hangneu <shangneu1992@gmail.com>
2017-09-04 17:53:58 -0400

    Update 176.py

--- 176.py
+++ 176.py
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
-Select max(Salary) from Employee
+Select max(Salary) as SecondHighestSalary from Employee
 where Salary < (Select MAX(Salary) from Employee)
