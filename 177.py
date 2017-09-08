854ee17040fe7f27ce9ce61895f866593bb19319
hangneu <shangneu1992@gmail.com>
2017-09-07 22:13:19 -0400

    Create 177.py

---
+++ 177.py
@@ -0,0 +1,8 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+set N = N - 1;
+  RETURN (
+      # Write your MySQL query statement below.
+      select Salary from Employee group by Salary order by Salary desc limit N,1
+  );
+END
