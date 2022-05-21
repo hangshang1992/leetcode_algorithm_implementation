ac63a4abc169fa214605990daa9224cb06560621
hangshang1992 <shangneu1992@gmail.com>
2022-05-21 20:07:22 +0800

    Create 133.cpp

---
+++ 133.cpp
@@ -0,0 +1,42 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    unordered_map<Node*, Node*> umap;
+    Node* cloneGraph(Node* node) {
+        if (node == NULL){
+            return NULL;
+        }
+        return dfs(node);
+    }
+    Node* dfs(Node* node){
+        if (umap[node]){
+            return umap[node];
+        }
+        Node* clone = new Node(node->val);
+        umap[node] = clone;
+        for (Node* vec: node->neighbors){
+            clone->neighbors.push_back(dfs(vec));
+        }
+        return clone;
+    }
+};
