a4c46edecd63c3a4f90f676c764d4453a7f0d68d
hangshang1992 <shangneu1992@gmail.com>
2022-05-22 23:50:56 +0800

    Create 11.cpp

---
+++ 11.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int res=0, left = 0, right = height.size() - 1;
+        while (left < right){
+            res = max({res, min({height[left], height[right]}) * (right - left)});
+            if (height[right] > height[left]){
+                left++;
+            }else{
+                right--;
+            }
+        }
+        return res;
+    }
+};
