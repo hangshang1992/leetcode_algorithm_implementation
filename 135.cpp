a32fb821427657db866a294f745055088fa99180
hangshang1992 <shangneu1992@gmail.com>
2022-06-04 11:19:04 +0800

    Create 135.cpp

---
+++ 135.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        vector<int> candy_list(ratings.size(), 1);
+        for (int i = 0; i < ratings.size() - 1; i++)
+        {
+            if (ratings[i] < ratings[i + 1])
+            {
+                candy_list[i + 1] = candy_list[i] + 1;
+            }
+        }
+
+        for(int i = ratings.size() - 1; i >= 1; i--){
+            if(ratings[i-1] > ratings[i] and candy_list[i-1] <= candy_list[i]){
+                candy_list[i-1] = candy_list[i] + 1;
+            }
+        }
+        return accumulate(candy_list.begin(), candy_list.end(), 0);
+    }
+};
